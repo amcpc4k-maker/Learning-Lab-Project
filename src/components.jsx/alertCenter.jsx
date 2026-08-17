bb611821b6df16fba3a1641e64199bd85d73e489
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 01:59:50 -0500

    Add alertCenter component with input fields

--- src/components.jsx/alertCenter.jsx
+++ src/components.jsx/alertCenter.jsx
@@ -1,1 +1,21 @@
 
+
+
+
+export default function alertCenter() {
+  return (
+
+            <label>Alert Type</label>
+            <input></input>
+
+            <label>Alert Length</label>
+            <input></input>
+
+            <label></label>
+            <input></input>
+
+              
+            <button>create</button>
+
+       )
+    }
