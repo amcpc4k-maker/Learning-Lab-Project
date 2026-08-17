725b92edb055bc9dbd368fa61eea9d84cc39ffdd
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 01:36:17 -0500

    Add UserProfile class with user properties

--- backend/UserProfile.java
+++ backend/UserProfile.java
@@ -1,1 +1,13 @@
 
+
+
+
+public class UserProfile {
+
+      public String userName;
+      public String colorChoice;
+      public int networkRating;
+      public String myNotifications = "";
+
+  
+}
