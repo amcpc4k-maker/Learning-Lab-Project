3cf7db930c7fba615ed3c6ff318c9e5b6fa0cefb
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 00:57:13 -0500

    Update ColorChoice.java

--- backend/ColorChoice.java
+++ backend/ColorChoice.java
@@ -1,1 +1,17 @@
 
+
+
+
+public class ColorChoice {
+
+    public String colorChoice = "";
+    public String colorEffect = "";
+    public int satMax;
+    public int satMin;
+    public int hueMax;
+    public int hueMin;
+    public int brightMax;
+    public int brightMin;
+
+  
+}
