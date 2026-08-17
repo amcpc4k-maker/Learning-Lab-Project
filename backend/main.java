6bfef896b7f8a153a703854001c61e9b555bbc90
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 00:30:49 -0500

    Update main.java

--- backend/main.java
+++ backend/main.java
@@ -1,1 +1,13 @@
 
+
+
+public class main {
+  public static void main(String[] arga) {
+
+          public String colorChoice = "";
+          public double tempRange;
+          public double durationLength;
+    
+    
+     }
+  }
