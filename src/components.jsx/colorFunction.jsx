712cded13bf36bf2b41cae1f48d5581c5ded882a
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-16 22:22:14 -0500

    Create colorFunction.jsx

---
+++ src/components.jsx/colorFunction.jsx
@@ -0,0 +1,39 @@
+import './index.css';
+
+
+
+export colorFunction() {
+  return (
+
+    <div>
+      <label>Pick an overhead color</label>
+      <select>
+        <option>Red</option>
+         <option>Purple</option>
+         <option>Green</option>
+         <option>Blue</option>
+         <option>Orange</option>
+         <option>Yellow</option>
+         <option>Black</option>
+         <option>White</option>
+      </select>
+
+    <div>
+      <h2>Pick background overlay color:</h2>
+       <select>
+        <option>Red</option>
+         <option>Purple</option>
+         <option>Green</option>
+         <option>Blue</option>
+         <option>Orange</option>
+         <option>Yellow</option>
+         <option>Black</option>
+         <option>White</option>
+      </select>
+    </div>
+      
+      
+    </div>
+
+    )
+}
