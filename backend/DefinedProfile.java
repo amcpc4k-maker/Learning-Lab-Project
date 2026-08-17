f17117f7605c59fd91ce7e8a18e3ddffcd56e2c8
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 01:02:57 -0500

    Update DefinedProfile.java

--- backend/DefinedProfile.java
+++ backend/DefinedProfile.java
@@ -1,1 +1,14 @@
 
+
+
+
+public class DefinedProfile {
+
+      public String userName = "";
+      public String passWord = "";
+      public String networkStatus = "";
+      public double tempRange;
+      public String colorChoice = "";
+      public int durationLength;
+  
+}
