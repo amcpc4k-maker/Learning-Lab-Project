8a869156a10d6a6227c074d499dcdd45dadfd769
amcpc4k-maker <amcpc4k@gmail.com>
2026-08-17 01:25:24 -0500

    Add temperature properties to TempPath class
    Added fields for temperature range, max, min, overload, and color variance.

--- backend/TempPath.java
+++ backend/TempPath.java
@@ -1,1 +1,14 @@
 
+
+
+
+public class TempPath {
+
+    public double tempRange;
+    public double tempMax;
+    public double tempMin;
+    public double tempOverload;
+    public int colorVariance;
+  
+  
+}
